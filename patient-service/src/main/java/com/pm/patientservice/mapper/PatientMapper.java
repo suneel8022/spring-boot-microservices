09773353cb9a939677e20a34849b3ec52d9e5b7b
Suneel <suneelhere8022@gmail.com>
2025-04-05 18:55:54 +0530

    Added PatientService Layer and included DTOs

---
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -0,0 +1,20 @@
+package com.pm.patientservice.mapper;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
+import org.springframework.beans.BeanUtils;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient){
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setName(patient.getName());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setDateOfBirth(patientDTO.getDateOfBirth());
+
+        return patientDTO;
+    }
+
+}
