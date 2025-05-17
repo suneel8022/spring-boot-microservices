6989ef823d0b1659ca06146593c3003990cdac22
Suneel <suneelhere8022@gmail.com>
2025-05-17 19:44:54 +0530

    Added an user model and docker support for auth-service

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,57 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID id;
+
+    @Column(unique = true,nullable = false)
+    public String email;
+
+    @Column(nullable = false)
+    public String password;
+
+    @Column(nullable = false)
+    public String role;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
+
+
