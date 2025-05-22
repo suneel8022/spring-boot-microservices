49c086502cce5faef8945039b84d5a473c9caeb8
Suneel <suneelhere8022@gmail.com>
2025-05-22 23:06:44 +0530

    Validate the JWT token to perform the token validation

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.pm.authservice.service;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -31,4 +32,14 @@ public class AuthService {
 
         return token;
     }
+
+    public boolean validateToken(String token){
+        try {
+            jwtUtil.validateToken(token);
+            return true;
+        }catch (JwtException e){
+            return false;
+        }
+    }
 }
+
